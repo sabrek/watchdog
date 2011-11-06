012dde5773cc57ced8ce6f490e458ddbec35fff3
Jacek Karolak <j.karolak@sabr.pl>
2011-11-06 14:09:07 +0100

    Initial commit, added initial loop.py

---
+++ loop.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# Author: Jacek 'Sabr' Karolak (j.karolak@sabr.pl)
+# Watch [files] for change, and after every change run given [command]
+
+import argparse, subprocess
+
+class Loop(object):
+    pass
